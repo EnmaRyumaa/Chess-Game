4986727f89f10a85b757bc8e073fa9d76bec2b8e
Fernando Augusto <fer.augustofs@gmail.com>
2024-08-13 12:02:31 -0300

    Start implement board

--- src/application/App.java
+++ src/application/App.java
@@ -3,7 +3,6 @@ package application;
 import boardgame.Position;
 public class App {
     public static void main(String[] args) throws Exception {
-        Position pos = new Position(3,5);
-        System.out.println(pos);
+        Board board = new Board(8, 8);
     }
 }
